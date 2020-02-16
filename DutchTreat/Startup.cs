d368082e0edf1a8e3a4d0847fc5ed8d3b5bb0be7
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2020-02-16 14:41:46 +0200

    add folders

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -21,10 +21,12 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          app.Run(async context =>
-          {
-            await context.Response.WriteAsync("Hello World!");
-          });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+
         }
     }
 }
